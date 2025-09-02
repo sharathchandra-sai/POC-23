45d7f9307218e5e6003cc2c4dbd477ae4252124c
sharathchandra-sai <kodati.sharath2000@gmail.com>
2025-09-02 13:47:44 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hi, CI/CD Pipeline,Project Completed Successfully Project-23";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
+}
